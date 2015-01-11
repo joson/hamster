07dcc5f5b8883049d28de097da8b6ec5cbef8266
joson <joson.prog@gmail.com>
2015-01-12 00:56:32 +0800

    PetriNet structure.

--- src/java/main/com/josonprog/common/petrinet/PetriNet.java
+++ src/java/main/com/josonprog/common/petrinet/PetriNet.java
@@ -3,13 +3,51 @@
  */
 package com.josonprog.common.petrinet;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import com.josonprog.common.graph.DefaultDigraph;
 import com.josonprog.common.graph.Digraph;
+import com.josonprog.common.graph.OrthNodeEntry;
 
 /**
  * @author Joson
  *
  */
-public class PetriNet extends Digraph<PetriNode> {
+public class PetriNet extends DefaultDigraph<PetriNode> 
+		implements Digraph<PetriNode> {
+	private Set<OrthNodeEntry<Place>> placeEntrySet = new HashSet<OrthNodeEntry<Place>>();
+	private Set<OrthNodeEntry<Transition>> transitionEntrySet = new HashSet<OrthNodeEntry<Transition>>();
+
+	protected OrthNodeEntry<Place> createPlaceEntry(Place place) {
+		return new OrthNodeEntry<Place>(place);
+	}
+
+	protected OrthNodeEntry<Transition> createTransitionEntry(Transition transition) {
+		return new OrthNodeEntry<Transition>(transition);
+	}
+	
+	@Override
+	public boolean addNode(PetriNode node) {
+		if (node instanceof Place) {
+			return this.placeEntrySet.add(this.createPlaceEntry((Place) node));
+		} else if (node instanceof Transition) {
+			return this.transitionEntrySet.add(this.createTransitionEntry((Transition) node));
+		} else {
+			return false;
+		}
+	}
+	
+	@Override
+	public boolean containsNode(PetriNode node) {
+		if (node instanceof Place) {
+			return this.placeEntrySet.contains(node);
+		} else if (node instanceof Transition) {
+			return this.transitionEntrySet.contains(node);
+		} else {
+			return false;
+		}
+	}
 	
 	
 }
